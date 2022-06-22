e35892f91fbf0e1a7d11a6ae6a9b1ae37020ffc3
wole aina <oyindamolaaina06@gmail.com>
2022-06-22 15:41:17 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+alert("My Name is Aina Oyindamola");
+alert("I am 5 ft 6 tall");
+alert("I am from Nigeria");
+
+console.log("My Name is Aina Oyindamola");
+console.log("I am 5 ft 6 tall");
+console.log("I am from Nigeria");
+
+
+
+// let Name = prompt("Please enter your name");
+// let Height = prompt("PLease enter your height");
+// let Country = prompt("Please enter your country");
+ 
